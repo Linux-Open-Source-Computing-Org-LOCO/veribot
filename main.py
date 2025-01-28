f2e9f0e33fc67b6b3c78f103136eede7d427bf49
Arturo Perez <perez.arturo2005@outlook.com>
2025-01-28 16:54:58 -0600

    creating yaml config file

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ class myClient(discord.Client):
 intents = discord.Intents.default()
 client = myClient(intents = intents)
 
+
+
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user} ({client.user.id})")
